f49d8f92643171b1a3b87ab2d8103e5316de62cd
gunshui <minqinyang@163.com>
2015-06-12 11:00:33 +0800

    6.12 11:00

--- Butterfly Effect/QQBrandClassViewController.m
+++ Butterfly Effect/QQBrandClassViewController.m
@@ -29,6 +29,7 @@
     [super viewDidLoad];
     NSLog(@"QQBrandClassViewController");
     NSLog(@"123");
+    NSLog(@"haha");
     
     // Do any additional setup after loading the view from its nib.
     
