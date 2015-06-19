49c77d5e729833ab166f0283998f252f0343b248
gunshui <minqinyang@163.com>
2015-06-19 10:19:09 +0800

    6.19  10.19

---
+++ Butterfly Effect/YYSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYSearchViewController.h
+//  Butterfly Effect
+//
+//  Created by lyan on 15/6/18.
+//  Copyright (c) 2015年 digime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYSearchViewController : UIViewController
+
+@end
